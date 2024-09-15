98a0c1a3a29344cc5a99eb8e96525345237acd57
dkrupski <128532062+dkrupski@users.noreply.github.com>
2024-09-15 20:58:30 +0200

    Renamed file

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import os
 from datetime import datetime
+
+directory = "files"
+
+filenames = os.listdir(directory)
+
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+
+    day = datetime.now().strftime("%Y-%m-%d")
+
+    new_filename = f"{filename[:-4]}-{day}.txt"
+
+    new_filepath = os.path.join(directory, new_filename)
+
+    os.rename(filepath, new_filepath)
+
+    print(f"Renamed {filename} to {new_filename}")
